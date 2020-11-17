d34e1c83d68962c255da56f55022071c0f8a828a
LaBoa99 <72893451+LaBoa99@users.noreply.github.com>
2020-11-17 16:17:59 -0600

    Set-up
    Preparando

---
+++ php/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+//Base de datos
+define('NOMBRE_SERVIDOR', 'localhost');
+define('NOMBRE_USUARIO', 'root');
+define('PASSWORD', '');
+define('NOMBRE_BD', 'imagesDB');
+
+//URLS
+define('SERVIDOR', 'http://localhost/imagesDB/');
+
+
+?>
+
+ 
